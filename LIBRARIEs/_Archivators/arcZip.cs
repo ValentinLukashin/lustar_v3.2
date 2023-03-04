b21f18f8cb256030636205a8e57acca2e9143568
Valentin <Valentin.Lukashin@gmail.com>
2023-03-04 11:21:04 +0200

    Добавлен проект _Archivators

---
+++ LIBRARIEs/_Archivators/arcZip.cs
@@ -0,0 +1,10 @@
+﻿namespace nlArchivators
+{
+    /// <summary>
+    /// Класс 'arhZip'
+    /// </summary>
+    /// <remarks>Элемент для работы с архиватором WinZip. Не наследуется</remarks>
+    public sealed class arhZip
+    {
+    }
+}
---
+++ LIBRARIEs/_Archivators/arhRar.cs
@@ -0,0 +1,10 @@
+﻿namespace nlArchivators
+{
+    /// <summary>
+    /// Класс 'arhRar'
+    /// </summary>
+    /// <remarks>Элемент для работы с архиватором WinRar. Не наследуется</remarks>
+    public sealed class arhRar
+    {
+    }
+}
