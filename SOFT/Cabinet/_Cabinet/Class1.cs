c14ac5ae818250c7b9ffd7ad086196f67ae9ced3
Valentin <Valentin.Lukashin@gmail.com>
2023-03-04 12:53:04 +0200

    Создание файлов проектов

---
+++ SOFT/Cabinet/_Cabinet/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Cabinet
+{
+    public class Class1
+    {
+    }
+}
