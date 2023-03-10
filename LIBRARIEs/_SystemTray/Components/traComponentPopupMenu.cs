d380bbf89e719c3494f187adb4233e613e82dfaf
Valentin <Valentin.Lukashin@gmail.com>
2023-03-10 17:58:50 +0200

    temp

---
+++ LIBRARIEs/_SystemTray/Components/traComponentPopupMenu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace nlFormTray
+{
+    /// <summary>
+    /// Класс 'crlComponentPopupMenu'
+    /// </summary>
+    /// <remarks>Всплывающее меню</remarks>
+    public class traComponentPopupMenu : ContextMenuStrip
+    {
+        #region = ДИЗАЙНЕРЫ
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public traComponentPopupMenu()
+        {
+            _mObjectAssembly();
+        }
+
+        #endregion ДИЗАЙНЕРЫ
+
+        #region = МЕТОДЫ
+
+        #region - Поведение
+
+        /// <summary>
+        /// Сборка объекта
+        /// </summary>
+        protected virtual void _mObjectAssembly()
+        {
+            #region /// Описание компонента
+
+
+            #endregion Описание компонента
+
+            Type vType = this.GetType();
+            Name = vType.Name;
+            _fClassNameFull = vType.Namespace + "." + vType.Name + ".";
+
+            return;
+        }
+        /// <summary>
+        /// Презентация объекта
+        /// </summary>
+        protected virtual void _mObjectPresentation()
+        {
+        }
+        /// <summary>
+        /// Выполняется при клике кнопки мыши по иконке в трее
+        /// </summary>
+        /// <param name="e"></param>
+
+
+        #endregion Поведение
+
+        #endregion МЕТОДЫ
+
+        #region = ПОЛЯ
+
+        #region - Атрибуты
+
+        /// <summary> 
+        /// Полное название класса
+        /// </summary>
+        protected string _fClassNameFull = "";
+
+        #endregion Атрибуты
+
+        #endregion ПОЛЯ
+    }
+}
---
+++ LIBRARIEs/_SystemTray/Components/traComponentPopupMenuItem.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+using System;
+using System.Windows.Forms;
+
+namespace nlFormTray
+{
+    /// <summary>
+    /// Класс 'crlComponentPopupMenuItem'
+    /// </summary>
+    /// <remarks>Пункт всплывающего меню</remarks>
+    public class traComponentPopupMenuItem : ToolStripMenuItem
+    {
+        #region = ДИЗАЙНЕРЫ
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public traComponentPopupMenuItem()
+        {
+            _mObjectAssembly();
+        }
+
+        #endregion ДИЗАЙНЕРЫ
+
+        #region = МЕТОДЫ
+
+        #region - Поведение
+
+        /// <summary>
+        /// Сборка объекта
+        /// </summary>
+        protected virtual void _mObjectAssembly()
+        {
+            #region /// Описание компонента
+
+            #endregion Описание компонента
+
+            Type vType = this.GetType();
+            Name = vType.Name;
+            _fClassNameFull = vType.Namespace + "." + vType.Name + ".";
+
+            return;
+        }
+        /// <summary>
+        /// Презентация объекта
+        /// </summary>
+        protected virtual void _mObjectPresentation()
+        {
+        }
+
+        #endregion Поведение
+
+        #endregion МЕТОДЫ
+
+        #region = ПОЛЯ
+
+        #region = Внутренние
+
+        /// <summary> 
+        /// Полное название класса
+        /// </summary>
+        protected string _fClassNameFull = "";
+
+        #endregion Внутренние
+
+        #endregion ПОЛЯ
+    }
+}
