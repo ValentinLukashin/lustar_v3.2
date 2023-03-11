e94460064d15b423651714e6018f74eb805cd345
Valentin <Valentin.Lukashin@gmail.com>
2023-03-11 08:41:35 +0200

    test

--- UTILITEs/ForApplications/MailRobot/Program.cs
+++ UTILITEs/ForApplications/MailRobot/Program.cs
@@ -17,6 +17,8 @@ namespace MailRobot
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            ///
         }
     }
 }
